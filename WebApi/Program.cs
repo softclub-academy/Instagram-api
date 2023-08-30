5d6ff32a05c6b5a9c34386fc22fb769437b6fb2c
Nurullo Sulaymonov <48081398+NurulloSulaymonov@users.noreply.github.com>
2023-08-30 17:49:49 +0500

    added docker

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -48,7 +48,7 @@ catch (Exception e)
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
