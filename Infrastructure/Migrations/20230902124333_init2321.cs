a87fa9b9dfe6d2dd01ef0a7113c64852d42c89fe
nazarovqurbonali <nazarovqurbonali4@gmail.com>
2023-09-04 14:48:13 +0500

    some changes

---
+++ Infrastructure/Migrations/20230902124333_init2321.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2321 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Occupation",
+                table: "UserProfiles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(45)",
+                oldMaxLength: 45);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "UserProfiles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(45)",
+                oldMaxLength: 45);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "UserProfiles",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "UserProfiles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(45)",
+                oldMaxLength: 45);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "About",
+                table: "UserProfiles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Occupation",
+                table: "UserProfiles",
+                type: "character varying(45)",
+                maxLength: 45,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "UserProfiles",
+                type: "character varying(45)",
+                maxLength: 45,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "UserProfiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "UserProfiles",
+                type: "character varying(45)",
+                maxLength: 45,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "About",
+                table: "UserProfiles",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
