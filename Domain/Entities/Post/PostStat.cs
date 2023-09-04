25001067ade1d8ab9567a401b1e1ff1ffef5ff31
Bahrulloif <bahrulloif@gmail.com>
2023-09-04 15:15:19 +0500

    post

--- Domain/Entities/Post/PostStat.cs
+++ Domain/Entities/Post/PostStat.cs
@@ -5,6 +5,11 @@ namespace Domain.Entities.Post;
 public class PostStat
 {
     [Key]
+
+
+
+
+    
     public int PostId { get; set; }
     public int LikeCount { get; set; }
     public Post Post { get; set; }
