f85d2907e8e2d12b07d8dd917ea3d3b10fbda14c
Shahrom_developer <shahrom909050409@gmail.com>
2023-09-09 09:14:15 +0500

    updatePostGetAndGetByIdAndGetFollowPost

--- Infrastructure/Services/PostService/PostService.cs
+++ Infrastructure/Services/PostService/PostService.cs
@@ -49,10 +49,10 @@ public class PostService : IPostService
                     Content = p.Content,
                     DatePublished = p.DatePublished.ToShortDateString(),
                     Images = _context.Images.Where(i => i.PostId == p.PostId).Select(i => i.ImageName).ToList(),
-                    PostLikeCount = s.LikeCount,
-                    PostView = v.ViewCount,
+                    PostLikeCount = p.PostLike.LikeCount,
+                    PostView = p.PostView.ViewCount,
                     CommentCount = p.PostComments.Count(),
-                    PostFavorite = p.PostFavorites.FirstOrDefault(k => k.PostId == p.PostId && k.UserId == u.Id) != null
+                    PostFavorite = p.PostFavorites.FirstOrDefault(k => k.PostId == p.PostId && k.UserId == p.UserId) != null
                 }).ToListAsync();
             var totalRecord = posts.Count();
             return new PagedResponse<List<GetPostDto>>(result, filter.PageNumber, filter.PageSize, totalRecord);
@@ -99,10 +99,7 @@ public class PostService : IPostService
         try
         {
             var posts = await (from p in _context.Posts
-                join u in _context.Users on p.UserId equals u.Id
-                join f in _context.FollowingRelationShips on u.Id equals f.FollowingId
-                join s in _context.PostLikes on p.PostId equals s.PostId
-                join v in _context.PostViews on p.PostId equals v.PostId
+                join f in _context.FollowingRelationShips on p.UserId equals f.FollowingId
                 where f.UserId == filter.UserId
                 select new GetPostDto()
                 {
@@ -112,10 +109,10 @@ public class PostService : IPostService
                     Content = p.Content,
                     DatePublished = p.DatePublished.ToShortDateString(),
                     Images = _context.Images.Where(i => i.PostId == p.PostId).Select(i => i.ImageName).ToList(),
-                    PostLikeCount = s.LikeCount,
-                    PostView = v.ViewCount,
+                    PostLikeCount = p.PostLike.LikeCount,
+                    PostView = p.PostView.ViewCount,
                     CommentCount = p.PostComments.Count(),
-                    PostFavorite = p.PostFavorites.FirstOrDefault(k => k.PostId == p.PostId && k.UserId == u.Id) != null
+                    PostFavorite = p.PostFavorites.FirstOrDefault(k => k.PostId == p.PostId && k.UserId == p.UserId) != null
                 }).ToListAsync();
 
             var totalRecord = posts.Count();
