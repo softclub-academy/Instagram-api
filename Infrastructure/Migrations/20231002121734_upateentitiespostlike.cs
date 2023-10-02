37e1b011d03371ae56381c9fb2fff104515cb094
Shodmon0406 <133884946+Shodmon0406@users.noreply.github.com>
2023-10-02 18:00:07 +0500

    updatelikeandfollowing

---
+++ Infrastructure/Migrations/20231002121734_upateentitiespostlike.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class upateentitiespostlike : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PostUserLikes_ListOfUserCommentLikes_ListOfUserCommentLikeId",
+                table: "PostUserLikes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PostUserLikes",
+                table: "PostUserLikes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PostUserLikes_ListOfUserCommentLikeId",
+                table: "PostUserLikes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PostUserLikes_UserId",
+                table: "PostUserLikes");
+
+            migrationBuilder.DropColumn(
+                name: "ListOfUserCommentLikeId",
+                table: "PostUserLikes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "PostUserLikes",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PostUserLikes",
+                table: "PostUserLikes",
+                columns: new[] { "UserId", "PostLikeId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PostUserLikes",
+                table: "PostUserLikes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "PostUserLikes",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ListOfUserCommentLikeId",
+                table: "PostUserLikes",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PostUserLikes",
+                table: "PostUserLikes",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostUserLikes_ListOfUserCommentLikeId",
+                table: "PostUserLikes",
+                column: "ListOfUserCommentLikeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostUserLikes_UserId",
+                table: "PostUserLikes",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PostUserLikes_ListOfUserCommentLikes_ListOfUserCommentLikeId",
+                table: "PostUserLikes",
+                column: "ListOfUserCommentLikeId",
+                principalTable: "ListOfUserCommentLikes",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20231002125334_updatefollowing.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatefollowing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_FollowingRelationShips_UserId",
+                table: "FollowingRelationShips");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "FollowingRelationShips",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text")
+                .OldAnnotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FollowingId",
+                table: "FollowingRelationShips",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text")
+                .OldAnnotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FollowingRelationShips_UserId_FollowingId",
+                table: "FollowingRelationShips",
+                columns: new[] { "UserId", "FollowingId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_FollowingRelationShips_UserId_FollowingId",
+                table: "FollowingRelationShips");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "FollowingRelationShips",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text")
+                .Annotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FollowingId",
+                table: "FollowingRelationShips",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FollowingRelationShips_UserId",
+                table: "FollowingRelationShips",
+                column: "UserId");
+        }
+    }
+}
